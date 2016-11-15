0b0f60b20ddae796750d0b9b28bff2882feb3e25
leolassence <leolassence@gmail.com>
2016-11-15 18:10:20 +0100

    setup basics get requests with express

--- server.js
+++ server.js
@@ -1,17 +1,39 @@
 'use strict';
 
-const http = require('http');
-const data = require('./data');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+let contacts = require('./data');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/api/contacts', (request, response) => {
+  if (!contacts) {
+    response.status(404).json({ message: 'No contact found' });
+  }
+
+  response.json(contacts);
+});
+
+app.get('/api/contacts/:id', (request, response) => {
+  const requestId = request.params.id;
+
+  let contact = contacts.filter(contact => {
+    return contact.id == requestId;
+  });
+
+  if (!contacts) {
+    response.status(404).json({ message: 'No contact found' });
+  }
+
+  response.json(contact[0]);
+});
 
 const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer((request, response) => {
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'application/json');
-  response.end('<h1>Hello World</h1>');
-});
-
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}`);
 });
