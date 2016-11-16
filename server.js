cae9f3920ecc9cbec3fb3f9e26b5c517f4a29e9b
leolassence <leolassence@gmail.com>
2016-11-16 01:05:37 +0100

    added mongoose and mlab remote database connection to the app

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const uriUtil = require('mongodb-uri');
+
 let contacts = require('./data');
+
+const mongodbUri = 'mongodb://leolassence:kjkszpj@ds153677.mlab.com:53677/scotch-node-restful-api';
+const mongooseUri = uriUtil.formatMongoose(mongodbUri);
+const dbOptions = {};
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -84,5 +91,11 @@ const hostname = 'localhost';
 const port = 3000;
 
 app.listen(port, hostname, () => {
+  mongoose.connect(mongooseUri, dbOptions, (error) => {
+    if (error) {
+      console.error(error);
+    }
+  });
+  
   console.log(`Server running at http://${hostname}:${port}`);
 });
