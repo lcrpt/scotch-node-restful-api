674cbc99c8bd9be8f2acdceff9235876d54932c8
leolassence <leolassence@gmail.com>
2016-11-16 13:05:54 +0100

    added single contact, put and delete api http actions with express router and mongoose

--- server.js
+++ server.js
@@ -19,76 +19,9 @@ app.use(cors());
 
 app.use('/api/contacts', require('./api/contacts/routes/post_contact'));
 app.use('/api/contacts', require('./api/contacts/routes/get_contacts'));
-
-// app.get('/api/contacts', (request, response) => {
-//   if (!contacts) {
-//     response.status(404).json({ message: 'No contact found' });
-//   }
-//
-//   response.json(contacts);
-// });
-//
-// app.get('/api/contacts/:id', (request, response) => {
-//   const requestId = request.params.id;
-//
-//   let contact = contacts.filter(contact => {
-//     return contact.id == requestId;
-//   })[0];
-//
-//   if (!contacts) {
-//     response.status(404).json({ message: 'No contact found' });
-//   }
-//
-//   response.json(contact[0]);
-// });
-//
-// app.post('/api/contacts/', (request, response) => {
-//   const contact = {
-//     id: contacts.length + 1,
-//     first_name: request.body.first_name,
-//     last_name: request.body.last_name,
-//     email: request.body.email,
-//     website: request.body.website,
-//   };
-//
-//   contacts.push(contact);
-//
-//   response.json(contact);
-// });
-//
-// app.put('/api/contacts/:id', (request, response) => {
-//   const requestId = request.params.id;
-//
-//   let contact = contacts.filter(contact => {
-//     return contact.id == requestId;
-//   })[0];
-//
-//   const index = contacts.indexOf(contact);
-//   const keys = Object.keys(request.body);
-//
-//   keys.forEach(key => {
-//     contact[key] = request.body[key];
-//   });
-//
-//   contacts[index] = contact;
-//
-//   response.json(contacts[index]);
-// });
-//
-// app.delete('/api/contacts/:id', (request, response) => {
-//   const requestId = request.params.id;
-//
-//   let contact = contacts.filter(contact => {
-//     return contact.id == requestId;
-//   })[0];
-//
-//   const index = contacts.indexOf(contact);
-//
-//   contacts.splice(index, 1);
-//
-//   response.json({ message: `User ${requestId} as been deleted` });
-// });
-
+app.use('/api/contacts', require('./api/contacts/routes/get_contact'));
+app.use('/api/contacts', require('./api/contacts/routes/put_contact'));
+app.use('/api/contacts', require('./api/contacts/routes/delete_contact'));
 
 const hostname = 'localhost';
 const port = 3000;
